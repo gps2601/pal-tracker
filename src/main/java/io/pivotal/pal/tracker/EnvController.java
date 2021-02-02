a7cfa6e19e883dd9605968404866e0889c734887
gsmit592 <gsmit592@ford.com>
2021-02-02 16:03:54 +0000

    adding solutions for configuration section

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,39 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.util.Collections.emptyMap;
+
+@RestController
+public class EnvController {
+    final private String port;
+    final private String memoryLimit;
+    final private String cfInstanceIndex;
+    final private String cfInstanceAddr;
+
+    public EnvController(
+            @Value("${port:8080}") String port,
+            @Value("${memory.limit:1024}") String memoryLimit,
+            @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+            @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        Map<String, String> env = new HashMap<>();
+        env.put("PORT", port);
+        env.put("MEMORY_LIMIT", memoryLimit);
+        env.put("CF_INSTANCE_INDEX", cfInstanceIndex);
+        env.put("CF_INSTANCE_ADDR", cfInstanceAddr);
+        return env;
+    }
+}
